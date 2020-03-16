5415cc9c32ff1a39c61d00cf5cce8a9d28b9f06f
dominickhemphill <dominickhemphill1@gmail.com>
2020-03-16 09:55:47 -0400

    added portfolio banner structure

--- assets/style.js
+++ assets/style.js
@@ -1,16 +1,26 @@
 $(document).ready(function(e){
     $win = $(window);
     $navbar = $('#header');
-    $toggle = $('.toggle-button')
+    $toggle = $('.toggle-button');
     var width = $navbar.width();
     toggle_onclick($win,$navbar,width);
 
+    // resize event
+    $win.resize(function(){
+        toggle_onclick($win,$navbar,width);
+    });
+
+    $toggle_click(function(e){
+        $navbar.toggleClass("toggle-left");
+    });
 
 });
 
 
 function toggle_onclick($win,$navbar,width){
-    if($win.width()<= 768){
-        $navbar.css({left:"-200px"});
+    if($win.width() <= 768){
+        $navbar.css({left:`-${width}px`});
+    } else {
+        $navbar.css({left:'0px'});
     }
 }
